770559402441d9c978df42ee4d273a4b5fc91ed2
Ezo <User@EZO>
2023-08-20 00:44:06 +0300

    Add project files.

---
+++ ShamsErpBeta/Forms/FrmCountry.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace ShamsErpBeta.Forms
+{
+    partial class FrmCountry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dgvCountry = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCountry)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnDel);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.dgvCountry);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Font = new System.Drawing.Font("Ara Hamah Homs", 16F);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel1.Size = new System.Drawing.Size(507, 527);
+            this.panel1.TabIndex = 0;
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label13.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label13.Font = new System.Drawing.Font("Ara Hamah Homs", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label13.Location = new System.Drawing.Point(0, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(507, 34);
+            this.label13.TabIndex = 34;
+            this.label13.Text = "بلاد الصنع";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(425, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 29);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "البلاد";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(14, 56);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(379, 36);
+            this.txtName.TabIndex = 36;
+            // 
+            // dgvCountry
+            // 
+            this.dgvCountry.AllowUserToAddRows = false;
+            this.dgvCountry.AllowUserToDeleteRows = false;
+            this.dgvCountry.AllowUserToResizeColumns = false;
+            this.dgvCountry.AllowUserToResizeRows = false;
+            this.dgvCountry.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCountry.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCountry.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvCountry.Location = new System.Drawing.Point(0, 147);
+            this.dgvCountry.Name = "dgvCountry";
+            this.dgvCountry.RowHeadersVisible = false;
+            this.dgvCountry.Size = new System.Drawing.Size(507, 380);
+            this.dgvCountry.TabIndex = 37;
+            this.dgvCountry.Click += new System.EventHandler(this.dgvCountry_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(144, 97);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(124, 44);
+            this.btnSave.TabIndex = 38;
+            this.btnSave.Text = "حفظ";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(14, 97);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(124, 44);
+            this.btnDel.TabIndex = 39;
+            this.btnDel.Text = "حذف";
+            this.btnDel.UseVisualStyleBackColor = true;
+            // 
+            // FrmCountry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 551);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmCountry";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "بلاد الصنع";
+            this.Load += new System.EventHandler(this.FrmCountry_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCountry)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dgvCountry;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ ShamsErpBeta/Forms/FrmCountry.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ShamsErpBeta.Forms;
+using ShamsErpBeta.Classes;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShamsErpBeta.Forms
+{
+    public partial class FrmCountry : Form
+    {
+        public FrmCountry()
+        {
+            InitializeComponent();
+        }
+
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+        int cr, id;
+
+        private void FrmCountry_Load(object sender, EventArgs e)
+        {
+            getFillData();
+        }
+
+        void getFillData()
+        {
+            var Sql = new SQLConClass();
+            ds = Sql.SelectData("Select Id, ROW_NUMBER() OVER (ORDER BY (SELECT 1)) ت, [Name] from TblCountry Where Del = 0", 0, default);
+            if (FunctionsClass.dsHasTables(ds))
+            {
+                dgvCountry.DataSource = ds.Tables[0];
+                dgvCountry.Columns[0].Visible = false;
+                dgvCountry.Columns[1].Width = 30;
+                dgvCountry.Columns[2].HeaderText = "البلد";
+            }
+        }
+
+        private void dgvCountry_Click(object sender, EventArgs e)
+        {
+            if (!FunctionsClass.checkDgvError(dgvCountry))
+                return;
+
+            cr = dgvCountry.CurrentRow.Index;
+            id = (int)dgvCountry.Rows[cr].Cells[0].Value;
+            txtName.Text = dgvCountry.Rows[cr].Cells[2].Value.ToString();
+            txtName.Focus();
+        }
+    }
+}
